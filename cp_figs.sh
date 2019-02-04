df08b6640f9ef2527a383788b4cb75f24b2f22af
stineb <benjamin.stocker@gmail.com>
2019-02-04 17:42:24 +0100

    don't remember

--- cp_figs.sh
+++ cp_figs.sh
@@ -10,16 +10,16 @@ cp ./fig//modobs_xdaily_Ty.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
 cp ./fig/modobs_spatial_annual_NULL.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
 cp ./fig/modobs_spatial_annual_FULL.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
 
-cp ./fig/meandoy_byzone/meandoy_byzone_Aw_south_FULL.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
-cp ./fig/meandoy_byzone/meandoy_byzone_BSk_north_FULL.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
-cp ./fig/meandoy_byzone/meandoy_byzone_Cfa_north_FULL.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
-cp ./fig/meandoy_byzone/meandoy_byzone_Cfb_north_FULL.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
-cp ./fig/meandoy_byzone/meandoy_byzone_Cfb_south_FULL.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
-cp ./fig/meandoy_byzone/meandoy_byzone_Csa_north_FULL.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
-cp ./fig/meandoy_byzone/meandoy_byzone_Csb_north_FULL.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
-cp ./fig/meandoy_byzone/meandoy_byzone_Dfb_north_FULL.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
-cp ./fig/meandoy_byzone/meandoy_byzone_Dfc_north_FULL.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
-cp ./fig/meandoy_byzone/meandoy_byzone_ET_north_FULL.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
+cp ./fig/meandoy_byzone/meandoy_byzone_Aw_south_.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
+cp ./fig/meandoy_byzone/meandoy_byzone_BSk_north_.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
+cp ./fig/meandoy_byzone/meandoy_byzone_Cfa_north_.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
+cp ./fig/meandoy_byzone/meandoy_byzone_Cfb_north_.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
+cp ./fig/meandoy_byzone/meandoy_byzone_Cfb_south_.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
+cp ./fig/meandoy_byzone/meandoy_byzone_Csa_north_.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
+cp ./fig/meandoy_byzone/meandoy_byzone_Csb_north_.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
+cp ./fig/meandoy_byzone/meandoy_byzone_Dfb_north_.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
+cp ./fig/meandoy_byzone/meandoy_byzone_Dfc_north_.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
+cp ./fig/meandoy_byzone/meandoy_byzone_ET_north_.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
 
 cp ./fig/modobs_anomalies_daily_NULL.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
 cp ./fig/modobs_anomalies_daily_FULL.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
--- plot_by_doy_allzones.R
+++ plot_by_doy_allzones.R
@@ -1,7 +1,9 @@
 ##------------------------------------------------------------
 ## Wrapper for mean seasonality by site (daily) for all climate zones
 ##------------------------------------------------------------
-plot_by_doy_allzones <- function( out_eval, out_eval1 = NA, out_eval2 = NA, makepdf = FALSE, label="", col2="royalblue", col3="darkgoldenrod", lab1="NA", lab2="NA", lab3="NA" ){
+plot_by_doy_allzones <- function( out_eval, out_eval1 = NA, out_eval2 = NA, out_eval3 = NA, 
+  makepdf = FALSE, label="", 
+  lab1="", lab2="", lab3="", lab4="", ... ){
   system( "mkdir -p fig/meandoy_byzone" )
 
   # tmp <- purrr::map( out_eval$data$meandoydf_byclim_stats$data, ~plot_by_doy_byzone(., out_eval1 = out_eval1, makepdf = makepdf, pattern = pattern ) )
@@ -9,9 +11,12 @@ plot_by_doy_allzones <- function( out_eval, out_eval1 = NA, out_eval2 = NA, make
   	as.list(seq(nrow(out_eval$data$meandoydf_byclim_stats))) , 
   	~plot_by_doy_byzone( 
   		out_eval$data$meandoydf_byclim_stats$data[[.]], 
-  		df2 = NA, #out_eval1$data$meandoydf_byclim_stats$data[[.]], 
-  		df3 = NA, #out_eval2$data$meandoydf_byclim_stats$data[[.]], 
-  		makepdf = makepdf, label=label, col2=col2, col3=col3, lab1=lab1, lab2=lab2, lab3=lab3 
+  		df2 = out_eval1$data$meandoydf_byclim_stats$data[[.]], 
+  		df3 = out_eval2$data$meandoydf_byclim_stats$data[[.]], 
+      df4 = ifelse(!is.na(out_eval3), out_eval3$data$meandoydf_byclim_stats$data[[.]], NA ),
+      lab1=lab1, lab2=lab2, lab3=lab3, lab4=lab4,
+      makepdf = makepdf, label=label, 
+  		...
   		) 
   	)
 }
@@ -20,8 +25,14 @@ plot_by_doy_allzones <- function( out_eval, out_eval1 = NA, out_eval2 = NA, make
 ##------------------------------------------------------------
 ## Mean seasonality with aggregated data from one climate zone 
 ##------------------------------------------------------------
-plot_by_doy_byzone <- function( df, df2 = NA, df3 = NA, makepdf = FALSE, label="", col2="royalblue", col3="darkgoldenrod", lab1="NA", lab2="NA", lab3="NA" ){
+plot_by_doy_byzone <- function( df, df2 = NA, df3 = NA, df4 = NA, 
+  makepdf = FALSE, label="", 
+  col2="royalblue", col3="darkgoldenrod", col4=add_alpha("springgreen3", 0.5), 
+  lab1="", lab2="", lab3="", lab4="" 
+  ){
 
+  col2 <- "royalblue"
+  
   if (df$nsites[1]>4){
   	dir_figs <- "./fig/meandoy_byzone"
   	if (makepdf && !dir.exists(dir_figs)) system( paste0( "mkdir -p ", dir_figs))
@@ -30,15 +41,29 @@ plot_by_doy_byzone <- function( df, df2 = NA, df3 = NA, makepdf = FALSE, label="
   	if (makepdf) pdf( filn )
       par(las=1)
       yrange <- range( df$mod_min, df$mod_max, df$obs_min, df$obs_max, na.rm = TRUE )
+
+      # obs
       plot(  df$doy, df$obs_mean, type="l", ylim = yrange, ylab = expression( paste("simulated GPP (gC m"^-2, "d"^-1, ")" ) ), xlab = "DOY" )
       polygon( c(df$doy, rev(df$doy)), c(df$obs_min, rev(df$obs_max)), border = NA, col = rgb(0,0,0,0.3)  )
+
+      # mod: FULL
       lines( df$doy, df$mod_mean, col="red", lwd=1.75 )
-      if (!identical(NA,df2)) lines( df2$doy, df2$mod_mean, col=col2, lwd=1.75, lty=1 )
-      if (!identical(NA,df3)) lines( df3$doy, df3$mod_mean, col=col3, lwd=1.75, lty=1 )
       polygon( c(df$doy, rev(df$doy)), c(df$mod_min, rev(df$mod_max)), border = NA, col = rgb(1,0,0,0.3)  )
+
+      # mod 2
+      if (!identical(NA,df2)) lines( df2$doy, df2$mod_mean, col=col2, lwd=1.75, lty=1 )
+
+      # mod 3
+      if (!identical(NA,df3)) lines( df3$doy, df3$mod_mean, col=col3, lwd=1.75, lty=1 )
+
+      # mod4
+      if (!identical(NA,df4)) lines( df4$doy, df4$mod_mean, col=col4, lwd=1.75, lty=1 )
+
       title( df$climatezone[1] )
+      
       mtext( bquote( italic(N) == .( df$nsites[1])), side=3, line=1, cex=1.0, adj=1.0 )
-      # legend("topright", c(lab1, lab2, lab3), col=c("red", col2, col3), lwd = c(1.75,1.75,1.75), bty = "n" )
+      legend("topright", c(lab1, lab2, lab3, lab4), col=c("red", col2, col3, col4), lwd = 1.75, bty = "n" )
+
     if (makepdf) dev.off()
   } else {
     rlang::warn( paste0("plot_by_doy_byzone(): Number of sites below 5 for climate zone ", df$climatezone[1] ) )
