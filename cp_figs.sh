efb28c3cbd74d0fc961b0def5dea59f21f081294
stineb <benjamin.stocker@gmail.com>
2019-06-04 10:11:35 -0700

    new figures

--- cp_figs.sh
+++ cp_figs.sh
@@ -1,41 +1,28 @@
 #!/bin/bash
 
-cp ./fig/modobs_xdaily_FULL.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
-cp ./fig/modobs_xdaily_NULL.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
+cp ./fig/modobs_xdaily.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
 
 cp ./fig//modobs_xdaily_DT.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/  
 cp ./fig//modobs_xdaily_NT.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/  
 cp ./fig//modobs_xdaily_Ty.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/  
 
-cp ./fig/modobs_spatial_annual_FULL.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
-cp ./fig/modobs_spatial_annual_NULL.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
-cp ./fig//modobs_spatial_annual_EVI.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/ 
+cp ./fig/modobs_spatial_annual.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
 
-cp ./fig/meandoy_byzone/meandoy_byzone_Aw_south_.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
-cp ./fig/meandoy_byzone/meandoy_byzone_BSk_north_.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
-cp ./fig/meandoy_byzone/meandoy_byzone_Cfa_north_.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
-cp ./fig/meandoy_byzone/meandoy_byzone_Cfb_north_.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
-cp ./fig/meandoy_byzone/meandoy_byzone_Cfb_south_.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
-cp ./fig/meandoy_byzone/meandoy_byzone_Csa_north_.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
-cp ./fig/meandoy_byzone/meandoy_byzone_Csb_north_.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
-cp ./fig/meandoy_byzone/meandoy_byzone_Dfb_north_.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
-cp ./fig/meandoy_byzone/meandoy_byzone_Dfc_north_.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
-cp ./fig/meandoy_byzone/meandoy_byzone_ET_north_.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
+cp ./fig/meandoy_byzone.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
+cp ./fig/meandoy_byzone_greenness.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
 
 cp ./fig/modobs_anomalies_daily_NULL.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
 cp ./fig/modobs_anomalies_daily_FULL.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
 cp ./fig/hist_anomalies_daily_FULL.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
 cp ./fig/hist_anomalies_daily_NULL.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
 
-cp fig/meandoy_byzone/meandoy_byzone_Dfb_north_greenness.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/ 
-cp fig/meandoy_byzone/meandoy_byzone_BSk_north_greenness.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/ 
-
 cp fig/functionalrel_gam_soilm.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
 cp fig/functionalrel_gam_temp.pdf  ~/Dropbox/projects/eval_pmodel/overleaf/fig/
 cp fig/functionalrel_gam_vpd.pdf   ~/Dropbox/projects/eval_pmodel/overleaf/fig/
 
+cp ./fig//modobs_spatial_annual_NULL.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/ 
 cp ./fig//modobs_spatial_annual_FULL.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/ 
-cp ./fig//modobs_spatial_annual_EVI.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/ 
+cp ./fig//modobs_spatial_annual_FULL_EVI.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/ 
 
 cp ./fig/functionalrel_gam_temp.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/ 
 cp ./fig/functionalrel_gam_vpd.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/ 
@@ -48,3 +35,7 @@ cp fig/map_sites.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
 cp ./fig/meandoy_byzone/meandoy_byzone_Cfb_north_Ty.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
 cp ./fig/meandoy_byzone/meandoy_byzone_Cfb_north_DT.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/
 cp ./fig/meandoy_byzone/meandoy_byzone_Cfb_north_NT.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/ 
+
+cp ./fig/modobs_lue_bysite_classid.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/ 
+cp ./fig/modobs_lue_monthly.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/ 
+cp ./fig/boxplot_lue_bysite_classid.pdf ~/Dropbox/projects/eval_pmodel/overleaf/fig/ 
