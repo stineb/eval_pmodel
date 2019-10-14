1041bd34ec7e6ada24000fc52babd0351568098d
Benjamin Stocker <benjamin.stocker@gmail.com>
2019-10-14 10:45:31 +0200

    added scripts

--- submit_calib_ORG.sh
+++ submit_calib_ORG.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 bsub -N -o ~/hpc_log/grid.out -n 3 -W 10:00 -R "rusage[mem=10000]" "R --vanilla --slave < eval_pmodel/rscript_calib_ORG.R >hpc_log/rscript_calib_ORG.Rout"
+
+
+# bsub -N -o ~/hpc_log/grid.out -n 3 -W 1:00 -R "rusage[mem=10000]" "R --vanilla --slave < eval_pmodel/rscript_calib_BRC.R >hpc_log/rscript_calib_BRC.Rout"
