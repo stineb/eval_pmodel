3a951206337901f5f79882cd00bac9526d3a6210
Benjamin Stocker <benjamin.stocker@gmail.com>
2019-10-21 14:21:30 +0200

    added submission files

--- submit_calib_ORG.sh
+++ submit_calib_ORG.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-bsub -N -o ~/hpc_log/grid.out -n 3 -W 10:00 -R "rusage[mem=10000]" "R --vanilla --slave < eval_pmodel/rscript_calib_ORG.R >hpc_log/rscript_calib_ORG.Rout"
-
-
-# bsub -N -o ~/hpc_log/grid.out -n 3 -W 1:00 -R "rusage[mem=10000]" "R --vanilla --slave < eval_pmodel/rscript_calib_BRC.R >hpc_log/rscript_calib_BRC.Rout"
-bsub -N -o ~/hpc_log/cluster.out -n 3 -W 18:00 -R "rusage[mem=10000]" "R --vanilla --slave < eval_pmodel/rscript_calib_FULL.R >hpc_log/rscript_calib_FULL.Rout"
+bsub -N -o ~/hpc_log/cluster_ORG.out  -n 3 -W 18:00 -R "rusage[mem=10000]" "R --vanilla --slave < ~/eval_pmodel/rscript_calib_ORG.R  >~/hpc_log/rscript_calib_ORG.Rout"
